ba46313d13d22b6ae7adba24eaffa52c30535056
Victor SIMON <victor.simon760@gmail.com>
2024-10-07 09:51:36 +0200

    deplacable / avant TP5

---
+++ WofE/src/main/java/lv/wofe/Deplacable.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package lv.wofe;
+
+/**
+ *
+ * @author lazaregrail & victorsimon
+ * 
+ */
+
+public interface Deplacable {
+    /**
+     * Permet de déplacer les Joueurs et PNJ.
+     * @param x est l'abscisse du déplacement
+     * @param y est l'ordonnée du déplacement
+     */
+    
+    void deplacer(int x , int y);
+}
+
